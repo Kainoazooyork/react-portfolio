c2f11c9240c20fa6674f44bdf6eacac9437c793e
Kainoa Furtado <144808230+Kainoazooyork@users.noreply.github.com>
2024-05-07 14:56:29 -0700

    Adding vite starter

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    
+ {/* Header */}
+
+      <main>
+        <sec>
+          <h2>About Me</h2>
+       </sec>
+       <sec>
+          <h2>Projects</h2>
+      </sec>
+      <sec>
+          <h2>Contact</h2>
+      </sec>
+
+      </main>
+
+      <footer>
+        <a href= "https://github.com/Kainoazooyork">Github</a>
+
+      </footer>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+
+
+function Header(){
+    
+    return (
+        <>
+        <header>
+        <h1>Portfolio</h1>
+          <nav>
+            <a href= ""></a>
+            <a href= ""></a>
+            <a href= ""></a>
+          </nav>
+
+      </header>
+      </>
+    )
+}
+
+export default Header

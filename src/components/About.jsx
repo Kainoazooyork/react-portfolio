7c6745f7b3b0add1761efcae5597f57a6285b34f
Kainoa Furtado <144808230+Kainoazooyork@users.noreply.github.com>
2024-05-14 16:08:35 -0700

    finished mvp

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,16 @@
 import reactImg from "../assets/react.svg"
+import selfie from "../assets/selfie.png"
 function About() {
 
     return (
         <>
             <section>
                 <h2>About Me</h2>
+                <section>
+                    <a href="https://github.com/kainoazooyork">
+                        <img src={selfie} alt=""className = "selfie" />
+                    </a>
+                </section>
                     <p>
                     Hey! I'm Kainoa Furtado. I am a 45 year old web developer. I enjoy spending time with my son and long motorcycle rides.
                     </p>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,8 @@
-// import project1 from "../assets/<filename>"
+import project1 from "../assets/project1.png"
+import project2 from "../assets/project2.png"
+import project3 from "../assets/project3.png"
+import project4 from "../assets/project4.png"
+import project5 from "../assets/project5.png"
 
 function Projects() {
 
@@ -8,35 +12,31 @@ function Projects() {
                 <h2>Projects</h2>
 
                 <section>
-                    <a href="">
-                        <img src="" alt="" />
+                    <a href="https://Kainoazooyork.github.io/weather-forecaster">
+                        <img src={project1} alt="" className = "projectImage"/>
                     </a>
                 </section>
                 <section>
-                    <a href="">
-                        <img src="" alt="" />
+                    <a href="https://Kainoazooyork.github.io/work-day-scheduler">
+                        <img src={project2} alt=""className = "projectImage" />
                     </a>
                 </section>
                 <section>
-                    <a href="">
-                        <img src="" alt="" />
+                    <a href="https://Kainoazooyork.github.io/timed-quiz">
+                        <img src={project3} alt=""className = "projectImage" />
                     </a>
                 </section>
                 <section>
-                    <a href="">
-                        <img src="" alt="" />
+                    <a href="https://gist.github.com/Kainoazooyork/70dfda1667839b9540b290624d5132d5">
+                        <img src={project4} alt="" className = "projectImage"/>
                     </a>
                 </section>
                 <section>
-                    <a href="">
-                        <img src="" alt="" />
+                    <a href="https://pwa-text-editor-zlr8.onrender.com/">
+                        <img src={project5} alt=""className = "projectImage" />
                     </a>
                 </section>
-                <section>
-                    <a href="">
-                        <img src="" alt="" />
-                    </a>
-                </section>
+                
             </section>
         </>
     )
